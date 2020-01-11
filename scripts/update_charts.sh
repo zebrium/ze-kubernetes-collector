0ccd7ff5df7b660a2d1114e78efcafdc7e001c21
Brady Zuo <brady@zebrium.com>
2020-01-10 18:31:48 -0800

    Update zlog-collector helm chart package to 1.18.0

---
+++ scripts/update_charts.sh
@@ -0,0 +1,6 @@
+cd ..
+#helm repo index zlog-collector --url  https://raw.githubusercontent.com/zebrium/ze-kubernetes-collector/master/
+cd ../chart
+helm package zlog-collector
+cd ..
+helm serve --repo-path ./charts --url https://raw.githubusercontent.com/zebrium/ze-kubernetes-collector/master/charts
