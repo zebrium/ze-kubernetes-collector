54f730bc4aa2de8901adad39a3526b31554c3fc3
Brady Zuo <brady@zebrium.com>
2020-01-12 18:46:53 -0800

    Make secret creation configurable and add a few more configurable parameters
    Now user can set "secret.enabled" to false to avoid secret from being created
    automatically during chart deployment.
    
    The following new configurable parameters are added:
    
    - verifySSL
    - excludePodRegex
    - tailFromHead
    - logFile
    - logLevel
    - nodeLogsPath
    - excludePath
    - flushInterval

--- scripts/update_charts.sh
+++ scripts/update_charts.sh
@@ -1,6 +1,6 @@
 cd ..
 #helm repo index zlog-collector --url  https://raw.githubusercontent.com/zebrium/ze-kubernetes-collector/master/
-cd ../chart
+cd ../charts
 helm package zlog-collector
 cd ..
 helm serve --repo-path ./charts --url https://raw.githubusercontent.com/zebrium/ze-kubernetes-collector/master/charts
