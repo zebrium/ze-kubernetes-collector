d3e087e0a0e188b629663a0ca62c4f9e1d03c149
Brady Zuo <brady@zebrium.com>
2020-12-10 11:40:08 -0800

    Make sure service account token is mounted on log collector container
    Some users got this error when deploying log collector:
    
    Invalid Kubernetes API v1 endpoint https://10.3.0.1:443/api: SSL_connect returned=1 errno=0 state=error: certificate verify failed (unable to get issuer certificate)
    
    Base on this page: https://github.com/kubernetes/kubernetes/issues/71571,
    some kubernetes deployments don't enable mounting service account token
    by default, we need to specify explicitly by setting
    automountServiceAccountToken to true on service account

--- scripts/update_charts.sh
+++ scripts/update_charts.sh
@@ -1,7 +1,4 @@
-cd ..
-#helm repo index zlog-collector --url  https://raw.githubusercontent.com/zebrium/ze-kubernetes-collector/master/
-cd ./charts
+cd ../charts
 helm package zlog-collector
 cd ..
-#helm serve --repo-path ./charts --url https://raw.githubusercontent.com/zebrium/ze-kubernetes-collector/master/charts
-helm serve --repo-path ./charts --url https://github.com/zebrium/ze-kubernetes-collector/releases/download/1.39.0
+helm serve --repo-path ./charts --url https://github.com/zebrium/ze-kubernetes-collector/releases/download/1.45.0
