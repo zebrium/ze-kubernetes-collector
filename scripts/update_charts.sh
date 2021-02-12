97807fdd1a629050de4367182498ad8876dfe4c6
Brady Zuo <brady@zebrium.com>
2021-02-11 16:16:18 -0800

    Use "default" as default deployment name

--- scripts/update_charts.sh
+++ scripts/update_charts.sh
@@ -1,4 +1,5 @@
 cd ../charts
+version=`cat zlog-collector/Chart.yaml | awk '/version: / { print $2 }'`
 helm package zlog-collector
 cd ..
-helm serve --repo-path ./charts --url https://github.com/zebrium/ze-kubernetes-collector/releases/download/1.46.2
+helm serve --repo-path ./charts --url https://github.com/zebrium/ze-kubernetes-collector/releases/download/$version
